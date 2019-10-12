f14b4ece80a26f11ae4c67a2bb1e6b7b11fa2f54
kyoung90 <kenneth.young90@gmail.com>
2019-10-12 00:38:39 -0400

    Added jwt authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+    devise_for :users,
+               path: '',
+               path_names: {
+                 sign_in: 'login',
+                 sign_out: 'logout',
+                 registration: 'signup'
+               },
+               controllers: {
+                 sessions: 'sessions',
+                 registrations: 'registrations'
+               }
+  end
 end
