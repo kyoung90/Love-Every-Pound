35f0cbc51e80f5c1438a6a97563c4ab3244cd317
kyoung90 <kenneth.young90@gmail.com>
2019-10-13 20:38:00 -0400

    Added show user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+    before_action :authenticate_user!
+
+    def show
+        if current_user.id.to_s == params[:id]
+            render json: current_user
+        else
+            render json: {error: "You can only look at your data!"}
+        end 
+    end 
+
+
 end
