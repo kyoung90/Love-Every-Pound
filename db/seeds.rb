aa0288e1f421f51636575d447b00595a6dd9d79f
kyoung90 <kenneth.young90@gmail.com>
2019-10-26 14:37:27 -0400

    Added inspiration quotes seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,7 +43,25 @@ steve = Author.create(name: "Steve Jobs")
 rob = Author.create(name: "Rob Siltanen")
 og = Author.create(name: "Og Mandino")
 george = Author.create(name: "George R.R. Martin")
+roy = Author.create(name: "Roy T. Bennett")
+israelmore = Author.create(name: "Israelmore Ayivor")
+amy = Author.create(name: "Amy Tan")
+fuyumi = Author.create(name: "Fuyumi Soryo")
+colleen = Author.create(name: "Colleen Hoover")
+bucchianeri = Author.create(name: "E.A. Bucchianeri")
+brittany = Author.create(name: "Brittany Burgunder")
+rachel = Author.create(name: "Rachel Joyce")
+germany = Author.create(name: "Germany Kent")
+patrick = Author.create(name: "Patrick Ness")
+emily = Author.create(name: "Emily Nagoski")
+lorin = Author.create(name: "Lorin Morgan-Richards")
+gretchen = Author.create(name: "Gretchen Rubin")
+donald = Author.create(name: "Donald Trump")
+ngansop = Author.create(name: "Roy A. Ngansop")
+alexandra = Author.create(name: "Alexandra Bracken")
 
+
+#fitness
 fitness_quote1 = Quote.create(content: "The last three or four reps is what makes the muscle grow. This area of pain divides a champion from someone who is not a champion.", author_id: arnold.id, preference_id: fitness.id)
 fitness_quote2 = Quote.create(content: "Success usually comes to those who are too busy to be looking for it.", author_id: henry.id, preference_id: fitness.id)
 fitness_quote3 = Quote.create(content: "All progress takes place outside the comfort zone.", author_id: michael.id, preference_id: fitness.id)
@@ -67,29 +85,28 @@ fitness_quote20 = Quote.create(content: "Success is walking from failure to fail
 
 
 # inspiration
-
 quotes = [
-    {content: "If I look back I am lost.", author_id: walt.id, preference_id: inspiration.id},
-    {content: "When the going gets tough, put one foot in front of the other and just keep going. Don’t give up.", author_id: winston.id, preference_id: inspiration.id},
-    {content: "What's done is done. What's gone is gone. One of life's lessons is always moving on. It’s okay to look back to see how far you’ve come but keep moving forward.", author_id: will.id, preference_id: inspiration.id},
-    {content: "Failure is a bend in the road, not the end of the road. Learn from failure and keep moving forward.", author_id: unknown.id, preference_id: inspiration.id},
-    {content: "If you really want to eat, keep climbing. The fruits are on the top of the tree. Stretch your hands and keep stretching them. Success is on the top, keep going.", author_id: vince.id, preference_id: inspiration.id},
-    {content: "Then she told me why a tiger is gold and black. It has two ways. The gold side leaps with its fierce heart. The black side stands still with cunning, hiding its gold between the trees, seeing and not being seen, waiting patiently for things to come. I did not learn to use my black side until after the bad man left me.", author_id: steve.id, preference_id: inspiration.id},
-    {content: "I don't know what lies ahead, but I want to keep going forever...", author_id: rob.id, preference_id: inspiration.id},
-    {content: "I'm not going to do it. I'm not going to wish for a perfect life. the things that knock you down in life are tests, forcing you to make a choice between giving in and remaining on the ground or wiping the dirt off and standing up even taller than you did before you were knocked down. I'm choosing to stand up taller. I'll probably get knocked down a few more times before this life is through with me, but I can guarantee you I'll never stay on the ground.", author_id: og.id, preference_id: inspiration.id},
-    {content: "Yes, a deep lesson from the postage stamp. It attaches itself to a moveable material, the envelope and gets going. A good relationship keeps you going forward; a bad one keeps you static. Attach yourself to someone who is also going forward and you will also get there.", author_id: , preference_id: inspiration.id},
-    {content: "When in doubt, throw doubt out and have a little faith....", author_id: , preference_id: inspiration.id},
-    {content: "It's the days you have every right to breakdown and fall apart, yet choose to show up anyway that matter most. Don't diminish the small steps that others can't see.", author_id: , preference_id: inspiration.id},
-    {content: "If I just keep putting one foot in front of the other, it stands to reason that I'm going to get there.", author_id: , preference_id: inspiration.id},
-    {content: "Never give up hope. All things are working for your good. One day, you'll look back on everything you've been through and thank God for it.", author_id: , preference_id: inspiration.id},
-    {content: "Do you believe there's hope at the end?' I ask. 'No,' she says simply, looking away. 'No, I don't, but I'm still going.' She eyes me. 'You coming with?", author_id: , preference_id: inspiration.id},
-    {content: "....there was always a way to get through a difficulty. If you just keep swimming, you’ll find your way. And when your brain wants to give up because there’s no land in sight, you keep swimming, not because you’re certain swimming will take you where you want to go, but to prove to yourself that you can still swim.", author_id: , preference_id: inspiration.id},
-    {content: "When you're an original it may take time for people to catch on. So keep at it and don't question yourself.", author_id: , preference_id: inspiration.id},
-    {content: "It’s easier to keep up than to catch up...", author_id: , preference_id: inspiration.id},
-    {content: "Following your convictions means you must be willing to face criticism from those who lack the same courage to do what is right. It’s called the road less traveled.", author_id: , preference_id: inspiration.id},
-    {content: "The hugest pile of challenges and hardships will not hold back a needle of the clock.", author_id: , preference_id: inspiration.id},
-    {content: "Relish the opportunity to be an outsider. Embrace that label.", author_id: , preference_id: inspiration.id},
-    {content: "In darkness, you only need to see just as far as you headlights extend. As long as you keep going, it is enough.", author_id: , preference_id: inspiration.id}
+    {content: "If I look back I am lost.", author_id: george.id, preference_id: inspiration.id},
+    {content: "When the going gets tough, put one foot in front of the other and just keep going. Don’t give up.", author_id: roy.id, preference_id: inspiration.id},
+    {content: "What's done is done. What's gone is gone. One of life's lessons is always moving on. It’s okay to look back to see how far you’ve come but keep moving forward.", author_id: roy.id, preference_id: inspiration.id},
+    {content: "Failure is a bend in the road, not the end of the road. Learn from failure and keep moving forward.", author_id: roy.id, preference_id: inspiration.id},
+    {content: "If you really want to eat, keep climbing. The fruits are on the top of the tree. Stretch your hands and keep stretching them. Success is on the top, keep going.", author_id: israelmore.id, preference_id: inspiration.id},
+    {content: "Then she told me why a tiger is gold and black. It has two ways. The gold side leaps with its fierce heart. The black side stands still with cunning, hiding its gold between the trees, seeing and not being seen, waiting patiently for things to come. I did not learn to use my black side until after the bad man left me.", author_id: amy.id, preference_id: inspiration.id},
+    {content: "I don't know what lies ahead, but I want to keep going forever...", author_id: fuyumi.id, preference_id: inspiration.id},
+    {content: "I'm not going to do it. I'm not going to wish for a perfect life. the things that knock you down in life are tests, forcing you to make a choice between giving in and remaining on the ground or wiping the dirt off and standing up even taller than you did before you were knocked down. I'm choosing to stand up taller. I'll probably get knocked down a few more times before this life is through with me, but I can guarantee you I'll never stay on the ground.", author_id: colleen.id, preference_id: inspiration.id},
+    {content: "Yes, a deep lesson from the postage stamp. It attaches itself to a moveable material, the envelope and gets going. A good relationship keeps you going forward; a bad one keeps you static. Attach yourself to someone who is also going forward and you will also get there.", author_id: israelmore.id, preference_id: inspiration.id},
+    {content: "When in doubt, throw doubt out and have a little faith....", author_id: bucchianeri.id, preference_id: inspiration.id},
+    {content: "It's the days you have every right to breakdown and fall apart, yet choose to show up anyway that matter most. Don't diminish the small steps that others can't see.", author_id: brittany.id , preference_id: inspiration.id},
+    {content: "If I just keep putting one foot in front of the other, it stands to reason that I'm going to get there.", author_id: rachel.id, preference_id: inspiration.id},
+    {content: "Never give up hope. All things are working for your good. One day, you'll look back on everything you've been through and thank God for it.", author_id: germany.id, preference_id: inspiration.id},
+    {content: "Do you believe there's hope at the end?' I ask. 'No,' she says simply, looking away. 'No, I don't, but I'm still going.' She eyes me. 'You coming with?", author_id: patrick.id, preference_id: inspiration.id},
+    {content: "....there was always a way to get through a difficulty. If you just keep swimming, you’ll find your way. And when your brain wants to give up because there’s no land in sight, you keep swimming, not because you’re certain swimming will take you where you want to go, but to prove to yourself that you can still swim.", author_id: emily.id, preference_id: inspiration.id},
+    {content: "When you're an original it may take time for people to catch on. So keep at it and don't question yourself.", author_id: lorin.id, preference_id: inspiration.id},
+    {content: "It’s easier to keep up than to catch up...", author_id: gretchen.id, preference_id: inspiration.id},
+    {content: "Following your convictions means you must be willing to face criticism from those who lack the same courage to do what is right. It’s called the road less traveled.", author_id: donald.id, preference_id: inspiration.id},
+    {content: "The hugest pile of challenges and hardships will not hold back a needle of the clock.", author_id: ngansop.id, preference_id: inspiration.id},
+    {content: "Relish the opportunity to be an outsider. Embrace that label.", author_id: donald.id, preference_id: inspiration.id},
+    {content: "In darkness, you only need to see just as far as you headlights extend. As long as you keep going, it is enough.", author_id: alexandra.id, preference_id: inspiration.id}
 ]
 
 Quote.create(quotes)
