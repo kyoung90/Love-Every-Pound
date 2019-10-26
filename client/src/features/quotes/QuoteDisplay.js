a042e3c6f7460ccde8e69f4c10d512847e406f68
kyoung90 <kenneth.young90@gmail.com>
2019-10-26 18:37:45 -0400

    Added loading screen to quotes

--- client/src/features/quotes/QuoteDisplay.js
+++ client/src/features/quotes/QuoteDisplay.js
@@ -1,25 +1,35 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import Quote from "./Quote";
-import {connect} from "react-redux"
-import {fetchRandomQuote} from "../../actions/quoteActions"
+import { connect } from "react-redux";
+import { fetchRandomQuote } from "../../actions/quoteActions";
+import LoadingScreen from "../../commons/LoadingScreen";
 
-const QuoteDisplay = (props) => {
+const QuoteDisplay = props => {
   useEffect(() => {
-    props.fetchRandomQuote()
-  }, [props.fetchRandomQuote])
+    props.fetchRandomQuote();
+  }, []);
+
+  if (props.loading) {
+    return <LoadingScreen content="Retrieving a new quote" />;
+  }
+
   return <Quote quote={props.quote}></Quote>;
 };
 
 let mapStateToProps = state => {
   return {
-    quote: state.quotes.currentQuote
-  }
-}
+    quote: state.quotes.currentQuote,
+    loading: state.quotes.loading
+  };
+};
 
 let mapDispatchToProps = dispatch => {
   return {
     fetchRandomQuote: () => dispatch(fetchRandomQuote())
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(QuoteDisplay);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(QuoteDisplay);
